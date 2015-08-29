41ccebdc544c82e2283b6107899433a43a1fe5f8
Will <Will@Will-PC>
2015-08-29 18:54:52 +1000

    Exercise 10

--- week1-010.CircleCircumference/src/Circumference.java
+++ week1-010.CircleCircumference/src/Circumference.java
@@ -1,10 +1,14 @@
-
 import java.util.Scanner;
-
+ 
 public class Circumference {
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
-        // Program your solution here 
+         
+        System.out.println("Type the radius:");
+        //int radius = Integer.parseInt(reader.nextLine());
+        double radius = reader.nextDouble();
+        System.out.println("");
+         
+        System.out.println("Circumference of the circle:" + 2 * Math.PI * radius);
     }
 }
