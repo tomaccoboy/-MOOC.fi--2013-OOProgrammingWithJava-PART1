59923c9e9c56e336b14dd77c30d32cb14a0c96ce
Will <Will@Will-PC>
2015-08-29 18:52:16 +1000

    Exercise 5

--- week1-005.SecondsOfTheYear/src/SecondsOfTheYear.java
+++ week1-005.SecondsOfTheYear/src/SecondsOfTheYear.java
@@ -1,16 +1,16 @@
 public class SecondsOfTheYear {
-
-    public static void main(String[] args) {   
+ 
+    public static void main(String[] args) {  
         // In below an incomplete version of the program. Please complete it!
-        
+         
         int daysInYear = 365;
-        int hoursInDay = 0;
-        int minutesInHour = 0;
-        int secondsInMinute = 0;
-        
-        int secondsInYear = daysInYear * hoursInDay;
-
+        int hoursInDay = 24;
+        int minutesInHour = 60;
+        int secondsInMinute = 60;
+         
+        int secondsInYear = daysInYear * hoursInDay * minutesInHour * secondsInMinute;
+ 
         System.out.println("There are " + secondsInYear + " seconds in a year");
     }
-
+ 
 }
