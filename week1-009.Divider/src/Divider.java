37d3fb6ab2260939d1726245b3a19b3344bf99f8
Will <Will@Will-PC>
2015-08-29 18:54:30 +1000

    Exercise 9

--- week1-009.Divider/src/Divider.java
+++ week1-009.Divider/src/Divider.java
@@ -1,11 +1,17 @@
-
 import java.util.Scanner;
-
+ 
 public class Divider {
-
+ 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
-        // Implement your program here. Remember to ask the input from user.
+         
+        System.out.println("Type a number:");
+        int numberOne = Integer.parseInt(reader.nextLine());
+         
+        System.out.println("Type another number:");
+        int numberTwo = Integer.parseInt(reader.nextLine());
+         
+        double result = 1.0 * numberOne / numberTwo;
+        System.out.println("Division: " + numberOne + " / " + numberTwo + " = " + result);
     }
 }
