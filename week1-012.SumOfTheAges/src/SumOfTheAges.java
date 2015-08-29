54763a742908affa7a51cafa5636bcc0a5fb7732
Will <Will@Will-PC>
2015-08-29 18:55:33 +1000

    Exercise 12

--- week1-012.SumOfTheAges/src/SumOfTheAges.java
+++ week1-012.SumOfTheAges/src/SumOfTheAges.java
@@ -1,11 +1,26 @@
-
 import java.util.Scanner;
-
+ 
 public class SumOfTheAges {
-
+ 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
-        // Implement your program here
+         
+        System.out.println("Type your name:");
+        String nameOne = reader.nextLine();
+        System.out.println("");
+         
+        System.out.println("Type your age:");
+        int ageOne = Integer.parseInt(reader.nextLine());
+        System.out.println("");
+         
+        System.out.println("Type your name:");
+        String nameTwo = reader.nextLine();
+        System.out.println("");
+         
+        System.out.println("Type your age:");
+        int ageTwo = Integer.parseInt(reader.nextLine());
+        System.out.println("");
+         
+        System.out.println(nameOne + " and " + nameTwo + " are " + (ageOne+ageTwo) + "years in total.");
     }
 }
