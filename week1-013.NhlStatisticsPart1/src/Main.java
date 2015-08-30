af0d461004bbf7f3f4308c0284fd7367dbf90ae2
tomaccoboy <tomaccoboy@gmail.com>
2015-08-30 12:07:47 +1000

    Exercise 10

--- week1-013.NhlStatisticsPart1/src/Main.java
+++ week1-013.NhlStatisticsPart1/src/Main.java
@@ -1,8 +1,14 @@
 
 import nhlstats.NHLStatistics;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+        Scanner reader = new Scanner(System.in);
+
+        System.out.println("Top ten points");
+        NHLStatistics.sortByPoints();
+        NHLStatistics.top(10);
     }
 }
