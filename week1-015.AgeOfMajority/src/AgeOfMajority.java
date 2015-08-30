141bc7296bd101d091fabaa4ee0a11ddac6ccf2a
tomaccoboy <tomaccoboy@gmail.com>
2015-08-30 14:17:10 +1000

    Exercise 15

--- week1-015.AgeOfMajority/src/AgeOfMajority.java
+++ week1-015.AgeOfMajority/src/AgeOfMajority.java
@@ -5,7 +5,14 @@ public class AgeOfMajority {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
-        // Type your program here 
+        
+        System.out.println("How old are you?");
+        int inputAge = Integer.parseInt(reader.nextLine());
+        
+        if (inputAge >= 18) {
+            System.out.println("You have reached the age of majority!");
+        } else {
+            System.out.println("You have not reached the age of majority yet!");
+        }
     }
 }
