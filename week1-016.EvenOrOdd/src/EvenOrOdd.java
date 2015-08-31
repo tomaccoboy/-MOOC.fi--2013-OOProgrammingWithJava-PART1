80d9c30239b65d5414c5936070ef053d6388ddf9
tomaccoboy <tomaccoboy@gmail.com>
2015-08-31 17:16:19 +1000

    Exercise 16

--- week1-016.EvenOrOdd/src/EvenOrOdd.java
+++ week1-016.EvenOrOdd/src/EvenOrOdd.java
@@ -5,9 +5,16 @@ public class EvenOrOdd {
 
     public static void main(String[] args) {
         Scanner reader = new Scanner(System.in);
-
-        // Type your program here
-
+        
+        System.out.println("Type a number:");
+        int num = Integer.parseInt(reader.nextLine());
+        int remainder = num % 2;
+        if (remainder == 0) {
+            System.out.println("Number " + num + " is even. ");
+        } else {
+            System.out.println("Number " + num + " is odd. ");
+        }
+        
         // NOTE:
         // Parity of a value can be easily checked with modulo-operator %.
         // Test the following:
