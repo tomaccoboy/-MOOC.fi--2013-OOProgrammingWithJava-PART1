d21860074bdb3f36ca89d8fbeeaaadfdf825b99f
tomaccoboy <tomaccoboy@gmail.com>
2015-09-03 17:01:11 +1000

    Exercise 23

--- week1-023.Temperatures/src/Temperatures.java
+++ week1-023.Temperatures/src/Temperatures.java
@@ -7,13 +7,21 @@ public class Temperatures {
 
         Scanner reader = new Scanner(System.in);
         // Write your code here. 
-
+        
+        while (true) {            
+            System.out.println("Type a decimal value: ");
+            double inputDec = Double.parseDouble(reader.nextLine());
+            
+            if ((inputDec >= -30) && (inputDec <= 40)) {
+                Graph.addNumber(inputDec);
+            }
+        }
         // Graph is used as follows:
-        Graph.addNumber(7);
-        double value = 13.5;
-        Graph.addNumber(value);
-        value = 3;
-        Graph.addNumber(value);
+        //Graph.addNumber(7);
+        //double value = 13.5;
+        //Graph.addNumber(value);
+        //value = 3;
+        //Graph.addNumber(value);
         // Remove or comment out these lines above before trying to run the tests.
     }
 }
